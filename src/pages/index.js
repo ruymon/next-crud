ac6d242c7d4c324751be33a86115636d9106daeb
Ruy Monteiro <ruystfrancis@gmail.com>
2022-01-21 08:00:18 -0300

    feat(tailwind): Project Config

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next CRUD</title> 
+      </Head>
+
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </>
+  )
+}
